d5088cdbfb95ca9b0f9ab191e7eb92f4bdd408d4
suraj <srjgupta17@gmail.com>
2021-09-22 16:03:11 +0545

    youtube Integrated

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
 import requests from "./request";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 let Row = ({ title, url }) => {
   let [movies, setMovies] = useState([]);
+  let [trailerId, setTrailerID] = useState("");
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   useEffect(() => {
     async function f() {
@@ -19,6 +30,19 @@ let Row = ({ title, url }) => {
     f();
   }, [url]);
 
+  async function getMovieTrailer(movie) {
+    let trailer = await movieTrailer(
+      movie?.name || movie?.title || movie?.original_title
+    )
+      .then((url) => {
+        return url;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    setTrailerID(trailer.split("https://www.youtube.com/watch?v=")[1]);
+  }
+
   return (
     <div className="Row">
       <h3 className="Row__title">{title}</h3>
@@ -32,10 +56,26 @@ let Row = ({ title, url }) => {
                 movie?.poster_path || movie?.backdrop_path
               }`}
               alt={movie?.name || movie?.title}
+              onClick={() => {
+                getMovieTrailer(movie);
+              }}
             />
           );
         })}
       </div>
+      {trailerId ? (
+        <YouTube
+          autoplay
+          videoId={trailerId}
+          opts={opts}
+          onReady={(e) => {
+            // e.target.pauseVideo();
+          }}
+          className="Row__youtube"
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
